0df9e40beea7ee2b8597cbdc10eeab926627e176
Rushikesh Patil <rushipatil1999@gmail.com>
2022-10-27 11:09:15 -0700

    Created Skeleton

--- src/Urinals.java
+++ src/Urinals.java
@@ -1,3 +1,7 @@
+package ICA8_Testing_rpatil29.src;
+
+import java.util.Scanner;
+
 /**
  * SER-515 ICA8 Urinals class
  *
@@ -6,7 +10,22 @@
  * @since 10-27-2022
  */
 public class Urinals {
-    public static void main(String[] args) {
 
+    public int goodString(String str)
+    {
+        System.out.println("Not yet Implemented");
+        return -1;
+    }
+    public static void main(String[] args) {
+        System.out.println("Main Function");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose: \n 1. File Input \n 2. String Input");
+        int choice = sc.nextInt();
+        if(choice==2){
+            //String input code here
+        } else if (choice==1) {
+            //File input code here
+
+        }
     }
 }
