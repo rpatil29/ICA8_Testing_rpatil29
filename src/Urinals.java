4392d68394025235e3732b7fe87d7f5a4ece334e
Rushikesh Patil <rushipatil1999@gmail.com>
2022-10-27 10:57:37 -0700

    Added first file

---
+++ src/Urinals.java
@@ -0,0 +1,12 @@
+/**
+ * SER-515 ICA8 Urinals class
+ *
+ * @author Rushikesh Patil, rpatil29@asu.edu
+ * @version 1.0
+ * @since 10-27-2022
+ */
+public class Urinals {
+    public static void main(String[] args) {
+
+    }
+}
